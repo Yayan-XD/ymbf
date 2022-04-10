22a641de7e9335c4600862ca584050b15263b537
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-11 01:34:00 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
    try:
-       os.system("git pull")
+       os.system("git pull");os.system("rm -rf results/CP/...");os.system("rm -rf results/OK/...")
        __import__("ymbf").cek_server()
    except Exception as e:
        exit(str(e))
