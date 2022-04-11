ec63288b9ed6ff708a9508de35c4400f15982e70
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-11 23:41:49 +0700

    Update c.py

--- c.py
+++ c.py
@@ -1,23 +1,24 @@
 #Ngapain bro Mau recode?
 #Mikir dong goblok Lo pikir gampang buat script
 
-import requests as tod
+import requests as r
 
-from src import loz as z
-from data import logo as x
 from rich.panel import Panel
 from rich import print as prints
 
+from src import loz as qq
+from data import logo as asy
+
 def cek_server():
-    x.henceut()
+    asy.henceut()
     try:
-        req = tod.get("https://pastebin.com/raw/2hKSXLwD").text
+        req = r.get("https://pastebin.com/raw/2hKSXLwD").text
         if "tidak" in req:
-            z.moch_yayan()
+            qq.moch_yayan()
         elif "error" in req:
             prints(Panel("""mohon maaf kepada user brute, script sedang bermasalah. tunggu beberapa saat waktu admin sedang berusaha memperbaiki nya 😉
 untuk mendapatkan info selanjutnya kunjungi situs web ini:[green] https://www.yayanxd.my.id[/] lalu klik ikon [green]WhatsApp[/]""",title="BERMASALAH"));exit()
         else:
             prints(Panel("[bold red]gagal menghubungkan ke server[/]"))
-    except tod.exceptions.ConnectionError:
+    except r.exceptions.ConnectionError:
         print("");prints(Panel("😔[bold red] gagal menghubungkan ke server, silahkan cek koneksi anda dan mainkan mode pesawat 5 detik."));exit()
