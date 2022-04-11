2bc093b2b546ab4ea6c690f97f840bce9caa1b70
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-12 01:54:33 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 from src import cok as x
 
-if __name__ == '__main__':
-    os.system('git pull')
+if __name__ == "__main__":
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
     x.cek_server()
