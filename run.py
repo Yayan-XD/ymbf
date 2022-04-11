30102d8760d89a09385f59d3218673ce4a423b75
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-12 00:30:41 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,6 @@
-import os, sys, shutil
-from src import loz as x
+import os
+from src import cok as x
 
-
-try: shutil.rmtree('data/__pycache__')
-except: pass
-try: shutil.rmtree('src/__pycache__')
-except: pass
-
-for filename in os.listdir('data'):
-	if filename[-3:] == 'pyc':
-		try: os.remove('data/'+filename)
-		except: pass
-
-for filename in os.listdir('src'):
-	if filename[-3:] == 'pyc':
-		try: os.remove('src/'+filename)
-		except: pass
-
-wkwkskkkwwk = x.moch_yayan()
-wkwkskkkwwk.start()
+if __name__ == '__main__':
+    os.system('git pull')
+    x.moch_yayan()
