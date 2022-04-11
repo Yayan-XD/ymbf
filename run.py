090b7421ecf798e5210b57d61dae221d53dfb8ee
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-11 23:42:57 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,24 @@
-#Hallo anak anjing:v
-import os
+#Ngapain bro Mau recode?
+#Mikir dong goblok Lo pikir gampang buat script
 
-if __name__ == "__main__":
-   try:
-       os.system("git pull");os.system("rm -rf results/CP/...");os.system("rm -rf results/OK/...")
-       __import__("c").cek_server()
-   except Exception as e:
-       exit(str(e))
+import requests as r
+
+from rich.panel import Panel
+from rich import print as prints
+
+from src import loz as qq
+from data import logo as asy
+
+def cek_server():
+    asy.henceut()
+    try:
+        req = r.get("https://pastebin.com/raw/2hKSXLwD").text
+        if "tidak" in req:
+            qq.moch_yayan()
+        elif "error" in req:
+            prints(Panel("""mohon maaf kepada user brute, script sedang bermasalah. tunggu beberapa saat waktu admin sedang berusaha memperbaiki nya 😉
+untuk mendapatkan info selanjutnya kunjungi situs web ini:[green] https://www.yayanxd.my.id[/] lalu klik ikon [green]WhatsApp[/]""",title="BERMASALAH"));exit()
+        else:
+            prints(Panel("[bold red]gagal menghubungkan ke server[/]"))
+    except r.exceptions.ConnectionError:
+        print("");prints(Panel("😔[bold red] gagal menghubungkan ke server, silahkan cek koneksi anda dan mainkan mode pesawat 5 detik."));exit()
