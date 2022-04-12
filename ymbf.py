5268b9f0d16b0c0537c256d334044a39ab15dac1
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-12 10:56:55 +0700

    Update ymbf.py

--- ymbf.py
+++ ymbf.py
@@ -1,9 +1,15 @@
+from data import logo as asy
+from src import cok as wq
+
+from rich import print as prints
+from rich.panel import Panel
+
 def cek_server():
     asy.henceut()
     try:
         r=requests.Session();req = r.get("https://pastebin.com/raw/2hKSXLwD").text
         if "tidak" in req:
-            moch_yayan()
+            wq.moch_yayan()
         elif "error" in req:
             prints(Panel("""mohon maaf kepada user brute, script sedang bermasalah. tunggu beberapa saat waktu admin sedang berusaha memperbaiki nya
 untuk mendapatkan info selanjutnya kunjungi situs web ini:[green] https://www.yayanxd.my.id[/] lalu klik ikon [green]WhatsApp[/]""",title="BERMASALAH"));exit()
