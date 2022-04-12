9ecab0774d544239692a7695fd70bfc2d03a5637
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-12 11:26:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
+#######################################################
+# Name           : Yayan Multi Brute Facebook (YMBF)  #
+# File           : run.py                             #
+# Author         : Moch Yayan Juan Alvredo XD.        #
+# Github         : https://github.com/dz-id           #
+# Facebook       : https://www.facebook.com/KM39453   #
+# Website        : https://www.yayanxd.my.id          #
+# Python version : 0.3                                #
+#######################################################
+
+############# DON'T REMOVE THIS FUNCTIONS #############
+
 import os
 from src import cok as yy
 
 if __name__ == "__main__":
-    os.system("git pull")
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
     yy.cek_server()
