7f9fb4e9841186907f9d05c228a623d9bf95d96d
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2022-04-12 10:55:13 +0700

    Create ymbf.py

---
+++ ymbf.py
@@ -0,0 +1,13 @@
+def cek_server():
+    asy.henceut()
+    try:
+        r=requests.Session();req = r.get("https://pastebin.com/raw/2hKSXLwD").text
+        if "tidak" in req:
+            moch_yayan()
+        elif "error" in req:
+            prints(Panel("""mohon maaf kepada user brute, script sedang bermasalah. tunggu beberapa saat waktu admin sedang berusaha memperbaiki nya
+untuk mendapatkan info selanjutnya kunjungi situs web ini:[green] https://www.yayanxd.my.id[/] lalu klik ikon [green]WhatsApp[/]""",title="BERMASALAH"));exit()
+        else:
+            prints(Panel("[bold red]gagal menghubungkan ke server[/]"))
+    except requests.exceptions.ConnectionError:
+        print("");prints(Panel("😔 [bold red] gagal menghubungkan ke server, silahkan cek koneksi anda dan mainkan mode pesawat 5 detik."));exit()
